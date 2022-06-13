5396b09bcfb8a54c93b68a673cba90a27388e565
Jaime Baltodano <jaime.baltodano@yahoo.com>
2022-06-13 16:55:30 -0600

    new version

--- src/api/AxiosInstance.ts
+++ src/api/AxiosInstance.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://api-basketball.p.rapidapi.com",
+  baseURL: process.env.REACT_APP_BASE_URL ?? "https://api",
   headers: {
     "Content-type": "application/json",
-    "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
-    "X-RapidAPI-Host": process.env.REACT_APP_API_HOST,
+    "X-RapidAPI-Key": process.env.REACT_APP_API_KEY ?? "none",
+    "X-RapidAPI-Host": process.env.REACT_APP_API_HOST ?? "none",
   },
 });
